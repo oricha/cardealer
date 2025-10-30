808b4028ae78088652a3686a57f43a2651d7ed03
Karel Muniz <oricha@gmail.com>
2025-10-30 22:50:55 +0100

    remove unused

--- backend/src/main/java/com/crashedcarsales/config/CacheConfig.java
+++
@@ -1,7 +0,0 @@
-package com.crashedcarsales.config;
-
-// Redis Cache Configuration - COMMENTED OUT
-// This class is disabled because Redis dependencies are commented out
-public class CacheConfig {
-    // All Redis-related configuration has been commented out
-}
